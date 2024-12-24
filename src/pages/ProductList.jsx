927768c366d83a5f8cdea79c66bc5b6ad4c0de61
이강산 <bybark4785@gmail.com>
2024-12-24 19:11:21 +0900

    fix: useLocation import

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 import { Box, Text, Heading, Spinner, Grid, GridItem, Highlight } from "@chakra-ui/react";
 import axios from "axios";
 
